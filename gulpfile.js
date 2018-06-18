ac85c64bbf122d88615f5c7936e75f5bc8a43b11
Steve Kuhn <SRKuhn@student.fullsail.edu>
2018-06-18 16:42:05 -0400

    Initial Project Setup
    All necessary files and HTML starter layout

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+
+
+gulp.task('default', function () {
+
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function () {
+    gulp.watch('/scss/**/*.scss', ['sass']);
+});
