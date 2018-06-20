3146d5ada737d772f33d3781ee20d2500bc18646
Steve Kuhn <SRKuhn@student.fullsail.edu>
2018-06-20 18:06:34 -0400

    Animations, JS, Finalized html
    Most of the animations are done except for the image info transform, JS should be done

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
 // Searchphotos search point
+const access = `948e6f9f13e390b27e8cbb13b1d3d123e0a006b2980e65aec99db8456bf6221a`;
+const endPoint = `https://api.unsplash.com/search/photos?client_id=${access}&query=planes`;
 
-const access = "";
-const endPoint = "";
+//Selectors
+const main = document.querySelector("main");
 
-function renderResult(data) {
-    const { results } = data
-    for (const result of results) {
-        console.log(result)
-    }
-}
+let content = "";
 
 fetch(endPoint)
-.catch( err => console.log(err))
-.then(res => res.json)
-.catch( err => (console.log(err)))
-.then(data => renderResult(data))
-.catch( err => console.log(err))
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        else {
+            return Promise.reject("Error!");
+        }
+    })
+    // Console line for reading API query output
+    //.then(data => console.log(data))
+    .then(data => dataCollector(data))
+    .catch(error => console.log("Error is", error));
+
+function dataCollector(data)
+{
+    data.results.forEach(result =>
+    {
+        content +=
+        `<figure>
+            <ul>
+                <li><a href="${result.user.links.html}">${result.user.name}</a></li>
+                <li>${result.likes}</li>
+            </ul>
+            <img src="${result.urls.regular}" alt="${result.description}">
+        </figure>`
+    })
+    main.innerHTML = content;
+}
